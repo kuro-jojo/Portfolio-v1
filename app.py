4f478d7e1d054456518b48cd29c611eaf8f7c358
Jonathan ASSAMAGAN <80401029+kuro-jojo@users.noreply.github.com>
2023-07-07 09:54:01 +0000

    add logging system

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import logging
+import socket
+from logging.handlers import SysLogHandler
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from src.models.UserMessage import UserMessage
@@ -12,10 +15,6 @@ from src.modules.form import fetch_contact_data
 
 app = Flask(__name__)
 
-# app.config[
-#     "SQLALCHEMY_DATABASE_URI"
-# ] = f"mysql+pymysql://{ os.getenv('MYSQL_USER') }:{ os.getenv('MYSQL_PASSWORD')}@{ os.getenv('MYSQL_HOST')}/{ os.getenv('MYSQL_DATABASE')}"
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///portfolio.db"
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = False
 db = UserMessage.db
@@ -24,6 +23,8 @@ db.init_app(app)
 
 @app.route("/")
 def home():
+    """ Send to homepage """
+    
     with open("user_visits.txt", "a") as f:
         f.write("IP : " + request.remote_addr)
         f.write("\t")
@@ -55,13 +56,40 @@ def contact():
     return redirect(url_for("home"))
 
 
-@app.before_first_request
-def init():
-    """Initialize some configurations"""
-    # create tables
-    with app.app_context():
-        db.create_all()
+# @app.before_request
+# def init():
+#     """Initialize some configurations"""
+#     # create tables
+#     with app.app_context():
+#         db.create_all()
 
+@app.before_request
+def log():
+    """ Logs message to a remote log viewer"""
+    if not paperTrailAppUrl and not paperTrailAppPort:
+        raise TypeError("An url and a port is requested")
+    
+    syslog = SysLogHandler(address=(paperTrailAppUrl, paperTrailAppPort))
+    print(paperTrailAppPort, paperTrailAppUrl)
+    syslog.addFilter(ContextFilter())
+
+    format = "%(asctime)s %(hostname)s - %(message)s"
+    formatter = logging.Formatter(format, datefmt="%b %d %H:%M:%S")
+    syslog.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(syslog)
+    logger.setLevel(logging.INFO)
+    message = request.headers.get('User-Agent').split(' ')[0]
+    logger.info(f"Browser : {message}")
+    
+class ContextFilter(logging.Filter):
+    hostname = socket.gethostname()
+
+    def filter(self, record):
+        record.hostname = ContextFilter.hostname
+        return True
 
 if __name__ == "__main__":
+    paperTrailAppUrl = os.getenv("PAPER_TRAIL_URL")
+    paperTrailAppPort = os.getenv("PAPER_TRAIL_PORT")
     app.run(host="0.0.0.0", debug=True)
